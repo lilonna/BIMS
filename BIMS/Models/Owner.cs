1f6762a1f5c9a9678e2cba9d8e35db241570bb70
lilonna <lidiyahaile60@gmail.com>
2025-02-20 23:41:07 +0300

    a

--- BIMS/Models/Owner.cs
+++ BIMS/Models/Owner.cs
@@ -4,40 +4,38 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace BIMS.Models;
-
-public partial class Owner
+namespace BIMS.Models
 {
-    [Key]
-    public int Id { get; set; }
+    public partial class Owner
+    {
+        [Key]
+        public int Id { get; set; }
 
-    [Required]
-    [StringLength(50)]
-    public string FullName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string FullName { get; set; }
 
-    public int OwnershipTypeId { get; set; }
+        public int OwnershipTypeId { get; set; }
 
-    [Column("TIN")]
-    public int Tin { get; set; }
+        [Column("TIN")]
+        public int Tin { get; set; }
 
-    public int DocumentId { get; set; }
+        [StringLength(255)] // Store only the document file name (not the file itself)
+        public string License { get; set; }
 
-    public bool Verified { get; set; }
+        public bool Verified { get; set; }
 
-    public DateOnly RegisteredDate { get; set; }
+        public DateOnly RegisteredDate { get; set; }
 
-    public bool IsActive { get; set; }
+        public bool IsActive { get; set; }
 
-    public bool IsDeleted { get; set; }
+        public bool IsDeleted { get; set; }
 
-    [InverseProperty("Owner")]
-    public virtual ICollection<Building> Buildings { get; set; } = new List<Building>();
+        [InverseProperty("Owner")]
+        public virtual ICollection<Building> Buildings { get; set; } = new List<Building>();
 
-    [ForeignKey("DocumentId")]
-    [InverseProperty("Owners")]
-    public virtual Documente Document { get; set; }
-
-    [ForeignKey("OwnershipTypeId")]
-    [InverseProperty("Owners")]
-    public virtual OwnershipType OwnershipType { get; set; }
+        [ForeignKey("OwnershipTypeId")]
+        [InverseProperty("Owners")]
+        public virtual OwnershipType OwnershipType { get; set; }
+    }
 }
